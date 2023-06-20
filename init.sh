fb4a4d7f1d94e63f0696b3212694655ed8894944
Ansgar Dietrichs <adietrichs@gmail.com>
2023-06-20 20:35:04 +0200

    adjust init.sh

--- init.sh
+++ init.sh
@@ -16,22 +16,12 @@ fi
 source venv/bin/activate
 echo "Venv activated"
 
-# if venv was just created, upgrade pip and install black
+# if venv was just created, upgrade pip and install package
 if [ $venv_created -eq 1 ]; then
     echo "Upgrading pip"
     pip install --upgrade pip
     echo "Pip upgraded"
-    echo "Installing black"
-    pip install black
-    echo "Black installed"
-fi
-
-# Check if requirements.txt exists
-if [ -f "requirements.txt" ]; then
-    # if requirements.txt has changed or venv was just created, install dependencies
-    if [ requirements.txt -nt venv ] || [ $venv_created -eq 1 ]; then
-        echo "Installing dependencies"
-        pip install -r requirements.txt
-        echo "Dependencies installed"
-    fi
+    echo "Installing package in dev mode"
+    pip install -e .[dev]
+    echo "Package installed"
 fi
